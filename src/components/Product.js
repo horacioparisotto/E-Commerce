c48064bbaf486bbd4986d6414197362bed010468
Hoacio Parisotto <parisottohoracio@gmail.com>
2024-05-23 16:02:08 -0300

    v1.1

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import { useState } from "react";
-import Image from "next/image";
 import Currency from "react-currency-formatter";
+import Image from "next/image";
+import primeTag from "../../public/prime-tag.png";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -15,9 +16,11 @@ function Product({ id, title, price, description, category, image }) {
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
       <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4 classname ="my-3">{title}</h4>
+      <h4 classname="my-3">{title}</h4>
 
       <div className="flex">
         {Array(rating)
@@ -26,20 +29,20 @@ function Product({ id, title, price, description, category, image }) {
             <StarIcon key={i} className="h-5 text-yellow-500" />
           ))}
       </div>
-      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <p className="text-xs mt-2 mb-2 line-clamp-2">{description}</p>
 
-      <div className="mb-5" >
+      <div className="mb-5">
         <Currency quantity={price} currency="USD" />
       </div>
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">
-          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <div className="w-12">
+            <Image src={primeTag} fill />
+          </div>
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
-      <button className="mt-auto button">
-        Add to Basket
-      </button>
+      <button className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
