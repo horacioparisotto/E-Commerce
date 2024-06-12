6072bd2552ddf0d28d4ecb3e4292aaf9ffbf44e7
Hoacio Parisotto <parisottohoracio@gmail.com>
2024-06-12 12:36:35 -0300

    Final Build

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -18,11 +18,13 @@ function ProductFeed({ products }) {
           />
         ))}
 
-      <img
-        className="md:col-span-full"
-        src="https://links.papareact.com/dyz"
-        alt=""
-      />
+      <div className="md:col-span-full flex justify-center">
+        <img
+          className="object-contain"
+          src="https://links.papareact.com/dyz"
+          alt=""
+        />
+      </div>
 
       <div className="md:col-span-2">
         {products
